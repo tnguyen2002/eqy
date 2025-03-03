26e7349c237dbc191a2858faace0bd93171afbb7
Anh (Tom) Nguyen <tnguyen65699@gmail.com>
2025-03-02 16:47:27 -0800

    removed random

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Chessboard } from "react-chessboard";
+import PlayRandomMoveEngine from "./components/engine";
 
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="p-4 bg-white shadow-lg rounded-lg">
-        <Chessboard boardWidth={600} id="defaultBoard" />
+        <PlayRandomMoveEngine />
       </div>
     </div>
   );
---
+++ src/components/engine.tsx
@@ -0,0 +1,189 @@
+import { useState } from "react";
+import { Chess, Piece, Square } from "chess.js";
+import { Chessboard } from "react-chessboard";
+
+interface SquareStyles {
+  background: string;
+  borderRadius?: string;
+}
+
+export default function PlayRandomMoveEngine() {
+  const [game, setGame] = useState(new Chess());
+  const [moveFrom, setMoveFrom] = useState("");
+  const [moveTo, setMoveTo] = useState<Square | null>(null);
+  const [showPromotionDialog, setShowPromotionDialog] = useState(false);
+  const [rightClickedSquares, setRightClickedSquares] = useState({});
+  const [moveSquares, setMoveSquares] = useState({});
+  const [optionSquares, setOptionSquares] = useState({});
+
+  function getMoveOptions(square: Square) {
+    const moves = game.moves({
+      square,
+      verbose: true,
+    });
+    if (moves.length === 0) {
+      setOptionSquares({});
+      return false;
+    }
+    const newSquares: Partial<Record<Square, SquareStyles>> = {};
+    moves.map((move) => {
+      newSquares[move.to] = {
+        background:
+          game.get(move.to) &&
+          game.get(move.to)?.color !== game.get(square)?.color
+            ? "radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)"
+            : "radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)",
+        borderRadius: "50%",
+      };
+      return move;
+    });
+    newSquares[square] = {
+      background: "rgba(255, 255, 0, 0.4)",
+    };
+    setOptionSquares(newSquares);
+    return true;
+  }
+
+  function onSquareClick(square: Square) {
+    setRightClickedSquares({});
+
+    // from square
+    if (!moveFrom) {
+      console.log("in moveFrom");
+      const hasMoveOptions = getMoveOptions(square);
+      if (hasMoveOptions) setMoveFrom(square);
+      return;
+    }
+
+    // to square
+    if (!moveTo) {
+      console.log("in move To");
+      // check if valid move before showing dialog
+      const foundMove = square in optionSquares;
+      console.log("foundMove", foundMove);
+      // not a valid move
+      if (!foundMove) {
+        // check if clicked on new piece
+        const hasMoveOptions = getMoveOptions(square);
+        // if new piece, setMoveFrom, otherwise clear moveFrom
+        setMoveFrom(hasMoveOptions ? square : "");
+        console.log("invalid move");
+        return;
+      }
+
+      // valid move
+      setMoveTo(square);
+
+      // if promotion move
+      if (
+        (foundMove.color === "w" &&
+          foundMove.piece === "p" &&
+          square[1] === "8") ||
+        (foundMove.color === "b" &&
+          foundMove.piece === "p" &&
+          square[1] === "1")
+      ) {
+        setShowPromotionDialog(true);
+        return;
+      }
+
+      // is normal move
+      const gameCopy = game;
+      const move = gameCopy.move({
+        from: moveFrom,
+        to: square,
+        promotion: "q",
+      });
+
+      // if invalid, setMoveFrom and getMoveOptions
+      if (move === null) {
+        const hasMoveOptions = getMoveOptions(square);
+        if (hasMoveOptions) setMoveFrom(square);
+        return;
+      }
+      setGame(gameCopy);
+      setMoveFrom("");
+      setMoveTo(null);
+      setOptionSquares({});
+      return;
+    }
+  }
+  function onPromotionPieceSelect(piece: Piece) {
+    // if no piece passed then user has cancelled dialog, don't make move and reset
+    if (piece) {
+      const gameCopy = game;
+      gameCopy.move({
+        from: moveFrom,
+        to: moveTo as string,
+        promotion: piece[1].toLowerCase() ?? "q",
+      });
+      setGame(gameCopy);
+    }
+    setMoveFrom("");
+    setMoveTo(null);
+    setShowPromotionDialog(false);
+    setOptionSquares({});
+    return true;
+  }
+  function onSquareRightClick(square) {
+    const colour = "rgba(0, 0, 255, 0.4)";
+    setRightClickedSquares({
+      ...rightClickedSquares,
+      [square]:
+        rightClickedSquares[square] &&
+        rightClickedSquares[square].backgroundColor === colour
+          ? undefined
+          : {
+              backgroundColor: colour,
+            },
+    });
+  }
+  return (
+    <div>
+      <Chessboard
+        id="ClickToMove"
+        animationDuration={200}
+        arePiecesDraggable={false}
+        position={game.fen()}
+        onSquareClick={onSquareClick}
+        onSquareRightClick={onSquareRightClick}
+        // onPromotionPieceSelect={onPromotionPieceSelect}
+        customBoardStyle={{
+          borderRadius: "4px",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+        }}
+        customSquareStyles={{
+          ...moveSquares,
+          ...optionSquares,
+          ...rightClickedSquares,
+        }}
+        promotionToSquare={moveTo}
+        showPromotionDialog={showPromotionDialog}
+      />
+      <button
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.reset();
+          });
+          setMoveSquares({});
+          setOptionSquares({});
+          setRightClickedSquares({});
+        }}
+      >
+        reset
+      </button>
+      <button
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.undo();
+          });
+          setMoveSquares({});
+          setOptionSquares({});
+          setRightClickedSquares({});
+        }}
+      >
+        undo
+      </button>
+    </div>
+  );
+}
